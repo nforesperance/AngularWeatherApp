4e04e06c4aee65529578af4429f14c282d912e45
unknown <nforesperance1@gmail.com>
2020-01-09 06:53:20 +0100

    8 async requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,46 +24,9 @@ export class AppComponent implements OnInit {
   ){}
   ngOnInit(){
     this._serve.getPosition().then(pos=> {
-        // this.getWeather1(pos)
         this.getWeather(pos)
-        this.getUrls(pos)
         
       });
-  }
-  getWeather(event){
-    this.loading = true
-       let url1 = `http://api.openweathermap.org/data/2.5/weather?lat=${event.lat}&lon=${event.lng}&${this.appid}`;
-       let url2 = `http://api.openweathermap.org/data/2.5/forecast?lat=${event.lat}&lon=${event.lng}&${this.appid}`;
-    let url3 =url1
-    this.http.get(url1).toPromise()
-      .then(current =>{
-        this.http.get(url2).toPromise()
-          .then(day =>{
-                  this.response = {current:current,days:day}   
-                  console.log(this.response);
-                   
-                  this.showmap = false  
-                  this.loading = false;
-                  this.load1 = false;
-                
-          })
-          .catch(err =>{
-            console.log("error");       
-          })  
-      })
-      .catch(err =>{
-        console.log("error");       
-      })
-  }
-  getWeather1(event){
-    this.loading = true
-       let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/0207b907a2bb4c9fa5065494935d5dda/${event.lat},${event.lng},1578264330?exclude=flags`;
-     return this.http.get(url).toPromise()
-    .then(data =>{
-      console.log(data)     
-    })
-    .catch(err =>console.log(err)
-    )
   }
   getData(url){
     return new Promise((resolve,reject)=>{
@@ -82,21 +45,31 @@ export class AppComponent implements OnInit {
     .then(data =>{
       console.log(data)
       this.allData = data 
+      this.response = {current:data[data.length-2],days:data[data.length-1],data}  
+        console.log(this.response);
+        
+      this.showmap = false  
+      this.loading = false;
+      this.load1 = false;
     })
   }
-  getUrls(pos){
+  getWeather(pos){
+    this.loading = true
     let today = new Date()
     let day = today.getDay()
     let date = today.getDate()
     let urls =[]
-    for (let i = day; i>0; i--){
-       let dt = today.setDate(date-i)
+    for (let i = 6; i>=0; i--){
+        let dt = today.setDate(6-i)
        let str =  (today.getTime()/1000).toString();
        let time = str.substring(0,str.indexOf("."));     
        let url = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/0207b907a2bb4c9fa5065494935d5dda/${pos.lat},${pos.lng},${time}?exclude=currently,flags`
-       urls.push(url)
+       urls.push(url)  
     }
-    urls.push(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/0207b907a2bb4c9fa5065494935d5dda/${pos.lat},${pos.lng}?extend=hourly`)
+    let current = `http://api.openweathermap.org/data/2.5/weather?lat=${pos.lat}&lon=${pos.lng}&${this.appid}`;
+    let url2 = `http://api.openweathermap.org/data/2.5/forecast?lat=${pos.lat}&lon=${pos.lng}&${this.appid}`;
+    urls.push(current)
+    urls.push(url2)
     this.urls =urls
     this.loadData()
   }
