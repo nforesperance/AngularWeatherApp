3f8ff95529416161478a0ff1a1a0847e304402f3
unknown <nforesperance1@gmail.com>
2020-01-10 15:47:38 +0100

    routing begins

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { HomeComponent } from './home/home.component';
+import { SignupComponent } from './signup/signup.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"login",component:LoginFormComponent},
+  {path:"home",component:HomeComponent},
+  {path:"signup",component:SignupComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
